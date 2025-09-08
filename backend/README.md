dc7083a3c7a83b6d1f0f9c497415760df517c872
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2025-09-08 10:38:12 -0800

    feat: demo portal UI flow for funders

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+# NocoDB REST API (Instructions)
+
+- Auth Layer
+- Sync Logic
+- Email Reminder
---
+++ frontend/app/page.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://aksbdc.org/services/ai-resource-center/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          AI Resource Program
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://aksbdc.org/services/workshops/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Workshops
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://chatgpt.com/g/g-67afc46c21d48191b946aa82c64108ec-funding-sources-database-alaska-sbdc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to Funding Sources Database →
+        </a>
+      </footer>
+    </div>
+  );
+}
